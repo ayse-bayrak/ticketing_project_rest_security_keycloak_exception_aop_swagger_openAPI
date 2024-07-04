000e6d8f1f51d5b77780cd5e34cc4a6c895b9478
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-04 14:36:05 +0300

    Ticketing_project_rest implementation is started

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,40 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+//@Where(clause = "is_deleted=false")         // SELECT * FROM users WHERE id = 4 AND is_deleted = false;
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    private boolean enabled;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}
